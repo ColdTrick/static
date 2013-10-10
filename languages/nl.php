ef030d0407ac63f9dfdb93efc858f0f60d8b283b
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-10-10 15:21:26 +0200

    updated dutch language

--- languages/nl.php
+++ languages/nl.php
@@ -1,6 +1,8 @@
 <?php
 $language = array (
-	'item:object:static' => "Statische pagina",
+  'admin:appearance:static:new:parent' => 'Selecteer bovenliggende pagina',
+  'admin:appearance:static:new:parent:top_level' => 'Geen bovenliggende pagina',
+  'item:object:static' => 'Statische pagina',
   'static:admin:empty' => 'Geen statische pagina\'s gemaakt',
   'admin:appearance:static' => 'Beheer statische pagina\'s',
   'admin:appearance:static:new' => 'Maak/Bewerk een statische pagina',
