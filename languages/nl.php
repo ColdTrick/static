879f3394474f392cc8a0585b9c37630073111cfe
Jerome Bakker <jeabakker@coldtrick.com>
2014-05-05 15:21:56 +0200

    updated: Dutch language

--- languages/nl.php
+++ languages/nl.php
@@ -1,13 +1,21 @@
 <?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-05-05 15:21
+ */
+
 $language = array (
+  'static:edit' => 'Maak/Bewerk een statische pagina',
+  'static:new:comment' => 'Reacties toestaan',
+  'static:new:moderators' => 'Moderatoren toewijzen',
+  'static:add:subpage' => 'Maak een subpagina',
+  'static:revisions' => 'Revisies',
   'static:new:parent' => 'Selecteer bovenliggende pagina',
   'static:new:parent:top_level' => 'Geen bovenliggende pagina',
   'item:object:static' => 'Statische pagina',
   'static:admin:empty' => 'Geen statische pagina\'s gemaakt',
   'static:all' => 'Beheer statische pagina\'s',
-  'static:new' => 'Maak/Bewerk een statische pagina',
   'static:new:permalink' => 'Permalink',
   'static:add' => 'Maak een statische pagina',
-  'static:admin:create:subpage' => 'Maak een subpagina',
 );
 add_translation("nl", $language);
