7959133d589ca31a25125e781e55bfdec20bc022
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-09-10 11:27:52 +0200

    fixed correct redirect after removing a page in a group

--- actions/delete.php
+++ actions/delete.php
@@ -4,8 +4,14 @@ $guid = get_input("guid");
 if ($guid) {
 	$entity = get_entity($guid);
 	
+	$container = $entity->getContainerEntity();
+	
 	if (elgg_instanceof($entity, "object", "static") && $entity->canEdit()) {
 		$entity->delete();
+	}
+	
+	if ($container instanceof ElggGroup) {
+		forward("static/group/" . $container->guid);
 	}
 }
 
