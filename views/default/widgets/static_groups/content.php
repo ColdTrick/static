41e2e6c0c7d4a1c90fe9ed475ec090fbca56bf88
Jerome Bakker <jeabakker@coldtrick.com>
2014-11-26 14:35:45 +0100

    fixed: group access in widget for group owners

--- views/default/widgets/static_groups/content.php
+++ views/default/widgets/static_groups/content.php
@@ -6,6 +6,8 @@ if (empty($group) || !elgg_instanceof($group, "group")) {
 	return;
 }
 
+$can_write = $group->canWriteToContainer(0, "object", "static");
+
 $options = array(
 	"type" => "object",
 	"subtype" => "static",
@@ -16,4 +18,10 @@ $options = array(
 	"no_results" => elgg_echo("static:admin:empty")
 );
 
+if ($can_write) {
+	$ia = elgg_set_ignore_access(true);
+}
 echo elgg_list_entities($options);
+if ($can_write) {
+	elgg_set_ignore_access($ia);
+}
