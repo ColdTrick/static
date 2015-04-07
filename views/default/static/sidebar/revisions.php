546ec84e9dbec2f089f20f178ca515b3703dc535
Jerome Bakker <jeabakker@coldtrick.com>
2015-04-07 14:40:32 +0200

    fixed: revisions shows pagination

--- views/default/static/sidebar/revisions.php
+++ views/default/static/sidebar/revisions.php
@@ -2,7 +2,13 @@
 
 $entity = $vars["entity"];
 if ($entity) {
-	$annotations = elgg_list_annotations(array("guid" => $entity->getGUID(), "annotation_name" => "static_revision", "limit" => false));
+	$annotations = elgg_list_annotations(array(
+		"guid" => $entity->getGUID(),
+		"annotation_name" => "static_revision",
+		"limit" => false,
+		"pagination" => false
+	));
+	
 	if ($annotations) {
 		echo elgg_view_module("aside", elgg_echo("static:revisions"), $annotations);
 	}
