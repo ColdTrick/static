4a6e48c28f1027b65c7f305faf1ade032a3badcd
Jerome Bakker <jeabakker@coldtrick.com>
2015-04-14 11:47:47 +0200

    added: entity menu in full view

--- views/default/object/static.php
+++ views/default/object/static.php
@@ -1,16 +1,35 @@
 <?php
 
-$entity = $vars["entity"];
+$entity = elgg_extract('entity', $vars);
 
-if ($vars["full_view"]) {
-	$body = "";
+if (elgg_extract('full_view', $vars)) {
 	
+	$metadata = elgg_view_menu('entity', array(
+		'entity' => $vars['entity'],
+		'handler' => 'static',
+		'sort_by' => 'priority',
+		'class' => 'elgg-menu-hz alliander-theme-menu-static',
+	));
+	
+	$params = array(
+		'entity' => $entity,
+		'title' => false,
+		'metadata' => $metadata,
+		'tags' => false
+	);
+	$summary = elgg_view('object/elements/summary', $params);
+	
+	$body = "";
 	if ($entity->icontime) {
-		$body .= elgg_view_entity_icon($entity, "large", array("href" => false, "class" => "float-alt"));
+		$body .= elgg_view_entity_icon($entity, "large", array(
+			"href" => false,
+			"class" => "float-alt"
+		));
 	}
 	$body .= elgg_view("output/longtext", array("value" => $entity->description));
 	
 	echo elgg_view('object/elements/full', array(
+		'summary' => $summary,
 		'body' => $body,
 	));
 
@@ -20,25 +39,41 @@ if ($vars["full_view"]) {
 	$title = $entity->getVolatileData("search_matched_title");
 	$description = $entity->getVolatileData("search_matched_description");
 	
-	$title = elgg_view("output/url", array("text" => $title, "href" => $entity->getURL(), "is_trusted" => true));
+	$title = elgg_view("output/url", array(
+		"text" => $title,
+		"href" => $entity->getURL(),
+		"is_trusted" => true
+	));
 	$body = $title . "<br />" . $description;
 
 	echo elgg_view_image_block("", $body);
+	
 } elseif (elgg_in_context("widgets")) {
 	$body = elgg_view("output/url", array(
-			"text" => $entity->title,
-			"href" => $entity->getURL()
+		"text" => $entity->title,
+		"href" => $entity->getURL()
 	));
 	echo elgg_view_image_block("", $body);
+	
 } else {
 
 	$show_edit = elgg_extract("show_edit", $vars, true);
 	
 	$body = "<tr>";
-	$body .= "<td>" . elgg_view("output/url", array("text" => $entity->title, "href" => $entity->getURL(), "is_trusted" => true)) . "</td>";
+	$body .= "<td>" . elgg_view("output/url", array(
+		"text" => $entity->title,
+		"href" => $entity->getURL(),
+		"is_trusted" => true
+	)) . "</td>";
 	if ($show_edit && $entity->canEdit()) {
-		$edit_link = elgg_view("output/url", array("href" => "static/edit/" . $entity->getGUID(), "text" => elgg_view_icon("settings-alt")));
-		$delete_link = elgg_view("output/confirmlink", array("href" => "action/static/delete?guid=" . $entity->getGUID(), "text" => elgg_view_icon("delete")));
+		$edit_link = elgg_view("output/url", array(
+			"href" => "static/edit/" . $entity->getGUID(),
+			"text" => elgg_view_icon("settings-alt")
+		));
+		$delete_link = elgg_view("output/confirmlink", array(
+			"href" => "action/static/delete?guid=" . $entity->getGUID(),
+			"text" => elgg_view_icon("delete")
+		));
 	
 		$body .= "<td width='1%' class='center'>" . $edit_link . "</td>";
 		$body .= "<td width='1%' class='center'>" . $delete_link . "</td>";
