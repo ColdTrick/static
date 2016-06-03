48a14666f26d65fc89790a9a2ecde9aac6aae25d
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-06-03 11:56:28 +0200

    chore: rewrote code of pagehandler

--- classes/ColdTrick/StaticPages/PageHandler.php
+++ classes/ColdTrick/StaticPages/PageHandler.php
@@ -16,66 +16,50 @@ class PageHandler {
 	 */
 	public static function staticHandler($page) {
 		
-		$resource_loaded = false;
-		$vars = [];
-		
 		switch (elgg_extract(0, $page)) {
 			case 'view':
-				$resource_loaded = true;
-				
-				$vars['guid'] = (int) elgg_extract('1', $page);
-				
-				echo elgg_view_resource('static/view', $vars);
-				break;
+				echo elgg_view_resource('static/view', [
+					'guid' =>  (int) elgg_extract(1, $page)
+				]);
+				return true;
 			case 'edit':
-				$vars['guid'] = (int) elgg_extract('1', $page);
+				echo elgg_view_resource('static/edit', [
+					'guid' =>  (int) elgg_extract(1, $page)
+				]);
+				return true;
 			case 'add':
-				$resource_loaded = true;
-				
-				echo elgg_view_resource('static/edit', $vars);
-				break;
+				echo elgg_view_resource('static/edit');
+				return true;
 			case 'group':
-				$resource_loaded = true;
-				
-				$vars['guid'] = (int) elgg_extract('1', $page);
-				
-				if (elgg_extract('2', $page) === 'out_of_date') {
-					echo elgg_view_resource('static/out_of_date_group', $vars);
-				} else {
-					echo elgg_view_resource('static/group', $vars);
+				$resource = 'static/group';
+				if (elgg_extract(2, $page) === 'out_of_date') {
+					$resource = 'static/out_of_date_group';
 				}
-				break;
+				echo elgg_view_resource($resource, [
+					'guid' =>  (int) elgg_extract(1, $page)
+				]);
+				return true;
 			case 'out_of_date':
-				$resource_loaded = true;
 				
 				$user = false;
-				if (!empty($page[1])) {
-					$user = get_user_by_username($page[1]);
+				$username = elgg_extract(1, $page);
+				if (!empty($username)) {
+					$user = get_user_by_username($username);
 				}
 				
 				if ($user instanceof \ElggUser) {
-					$vars['user'] = $user;
 					elgg_set_page_owner_guid($user->getGUID());
 					
-					echo elgg_view_resource('static/out_of_date_owner', $vars);
+					echo elgg_view_resource('static/out_of_date_owner', ['user' => $user]);
 				} else {
-					echo elgg_view_resource('static/out_of_date', $vars);
+					echo elgg_view_resource('static/out_of_date');
 				}
-				break;
+				return true;
 			case 'all':
 			default:
-				$resource_loaded = true;
-				
-				echo elgg_view_resource('static/all', $vars);
-				break;
+				echo elgg_view_resource('static/all');
+				return true;
 		}
-		
-		// did we handle the page
-		if ($resource_loaded) {
-			return true;
-		}
-		
-		return false;
 	}
 	
 	/**
