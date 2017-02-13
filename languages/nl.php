fce254efb0e9ac146c3c232cd1ffee58002fa988
Jeroen Dalsem <jdalsem@coldtrick.com>
2017-02-13 12:20:02 +0100

    chore: updated dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,15 +1,37 @@
 <?php
 /**
- * This file was created by Translation Editor v4.0
- * On 2015-06-09 15:08
+ * This file was created by Translation Editor v5.2
+ * On 2017-02-13 12:19
  */
 
 return array (
+  'admin:upgrades:static:migrate_containers' => 'Migreer statische pagina containers',
+  'admin:upgrades:static:migrate_containers:description' => 'Dit migreert de containers van de statische pagina\'s zodat de pagina\'s netjes bij een groep of een site horen.',
+  'static:settings:general:title' => 'Algemene instellingen',
+  'static:settings:out_of_date:title' => 'Verouderde content instellingen',
+  'static:settings:out_of_date:description' => 'Met de verouderde content instellingen kun je een lijst krijgen met content die beschouwd wordt als \'verouderd\'. Er zullen ook notificaties verstuurd worden naar de persoon die als laatst de pagina heeft bewerkt, zodat die kan bepalen of de content no steeds relevant is.',
+  'static:settings:out_of_date_days:help' => 'Voer het aantal dagen in vanaf wanneer content beschouwd kan worden als \'verouderd\'. Bij de volledige weergave van de content zal ook een melding worden getoond dat de content \'verouderd\' is. 0 of leeg om deze functionaliteit uit te schakelen.',
+  'static:settings:out_of_date:reminder_interval' => 'Verstuur een herinnering na x dagen',
+  'static:settings:out_of_date:reminder_interval:help' => 'Voer het aantal dagen in waarna opnieuw wordt gecontroleerd of content nog steeds \'verouderd\' is. Indien dit is ingesteld, wordt de melding bij de content uitgesteld voor diegene die niet de content mag bewerken. 0 of leeg voor geen herinneringen.',
+  'static:settings:out_of_date:reminder_repeat' => 'Aantal keer dat er moet worden herinnerd',
+  'static:settings:out_of_date:reminder_repeat:help' => 'Vul hier een nummer in indien de herinneringsmail meerdere keren moet worden verzonden. 0 betekend geen herinneringen.',
+  'static:edit:menu:parent:direct_child' => 'Direct onder toppagina',
+  'static:out_of_date:message' => 'De content van deze pagina is mogelijk verouderd. Het kan zijn dat het niet meer relevant is.',
+  'static:out_of_date:message:mark' => 'Deze pagina is up-to-date.',
+  'static:out_of_date:notification:section:new' => 'Recent verouderde content',
+  'static:out_of_date:notification:section:reminder' => 'Herinnering %s voor verouderde content',
+  'static:csv_exporter:last_editor:guid' => 'Laatste editor (GUID)',
+  'static:csv_exporter:last_editor:name' => 'Laatste editor (naam)',
+  'static:csv_exporter:last_editor:username' => 'Laatste editor (gebruikersnaam)',
+  'static:csv_exporter:last_editor:email' => 'Laatste editor (email)',
+  'static:csv_exporter:last_editor:profile_url' => 'Laatste editor (url)',
+  'static:csv_exporter:last_revision:timestamp' => 'Laatste revisie timestamp',
+  'static:csv_exporter:last_revision:timestamp:readable' => 'Laatste revisie timestamp (leesbaar)',
+  'static:csv_exporter:out_of_date:state' => 'Is verouderd',
+  'static:group_tool_option:description' => 'Dit onderdeel is bedoeld voor zogenaamde ‘statische’ content. Dat wil zeggen informatie over jouw groep die je wilt delen met collega’s,  die relatief weinig verandert en die door de beheerders van de groep bewerkt kan worden.',
   'static:out_of_date:include_groups' => 'Inclusief groep content',
   'static:settings:enable_groups' => 'Schakel statisch pagina\'s voor groepen in',
-  'static:settings:enable_out_of_date' => 'Schakel verouderde content notificaties/weergave in',
   'static:settings:out_of_date_days' => 'Aantal dagen voordat content verouderd is',
-  'static:settings:out_of_date_days:days' => 'dagen',
   'static:menu:filter:out_of_date' => 'Alle verouderde content',
   'static:menu:filter:out_of_date:mine' => 'Mijn verouderde content',
   'static:menu:filter:out_of_date:group' => 'Groep verouderde content',
@@ -25,7 +47,6 @@ Een aantal van je statische pagina\'s zijn verouderd. Controleer of de informati
 
 Klik hier voor een complete lijst van de verouderde content
 %s',
-  'static:group_tool_option:description' => 'Dit onderdeel is bedoeld voor zogenaamde ‘statische’ content. Dat wil zeggen informatie over jouw groep die je wilt delen met collega’s,  die relatief weinig verandert en die door de beheerders van de groep bewerkt kan worden.',
   'static:new:thumbnail' => 'Upload afbeelding',
   'static:new:remove_thumbnail' => 'Verwijder afbeelding',
   'static:widgets:static_groups:title' => 'Statische pagina\'s',
