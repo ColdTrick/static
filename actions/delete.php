2bc7f93560ffb6a5268dfae834c6a9b7a7cd735c
Jerome Bakker <jeabakker@coldtrick.com>
2018-10-18 10:59:04 +0200

    fixed: permissions for moderators during deletion

--- actions/delete.php
+++ actions/delete.php
@@ -2,20 +2,27 @@
 
 $guid = (int) get_input('guid');
 if (empty($guid)) {
-	forward('static/all');
+	return elgg_error_response(elgg_echo('error:missing_data'));
 }
 	
 $ia = elgg_set_ignore_access(true);
 $entity = get_entity($guid);
 elgg_set_ignore_access($ia);
 
-if (elgg_instanceof($entity, 'object', 'static') && $entity->canEdit()) {
-	$entity->delete();
+if (!$entity instanceof StaticPage || !$entity->canDelete()) {
+	return elgg_error_response(elgg_echo('entity:delete:permission_denied'));
 }
 
+$title = $entity->getDisplayName();
 $container = $entity->getContainerEntity();
-if ($container instanceof ElggGroup) {
-	forward('static/group/' . $container->guid);
+
+if (!$entity->delete()) {
+	return elgg_error_response(elgg_echo('entity:delete:fail', [$title]));
 }
 
-forward('static/all');
+$forward_url = 'static/all';
+if ($container instanceof ElggGroup) {
+	$forward_url = "static/group/{$container->guid}";
+}
+
+return elgg_ok_response('', elgg_echo('entity:delete:success', [$title]), $forward_url);
