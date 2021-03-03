cefaa783f9ddccb415c6618d9f8d1c7b7bec2d1f
Jeroen Dalsem <jdalsem@coldtrick.com>
2021-03-03 15:41:09 +0100

    fixed: make sure owner can also see private content in out of date list

--- views/default/resources/static/out_of_date_owner.php
+++ views/default/resources/static/out_of_date_owner.php
@@ -19,39 +19,39 @@ if (!$page_owner->canEdit()) {
 
 elgg_push_collection_breadcrumbs('object', StaticPage::SUBTYPE);
 
-$dbprefix = elgg_get_config('dbprefix');
-$days = (int) elgg_get_plugin_setting('out_of_date_days', 'static');
-
-$options = [
-	'type' => 'object',
-	'subtype' => StaticPage::SUBTYPE,
-	'modified_time_upper' => time() - ($days * 24 * 60 * 60),
-	'wheres' => [
-		new WhereClause("e.guid IN (
-			SELECT revs.entity_guid
-			FROM (
-				SELECT *
-				FROM {$dbprefix}annotations a
-				JOIN (
-					SELECT entity_guid AS e_guid, MAX(time_created) AS max_time
-	                FROM {$dbprefix}annotations
-	                WHERE name = 'static_revision'
-	                GROUP BY entity_guid
-				) AS b ON (
-            		a.entity_guid = b.e_guid
-					AND a.time_created = b.max_time
-				)
-				WHERE a.owner_guid = {$page_owner->guid}
-			) revs
-		)"),
-	],
-	'order_by' => new OrderByClause('e.time_updated', 'DESC'),
-	'no_results' => elgg_echo('static:out_of_date:none'),
-];
-
 $title_text = elgg_echo('static:out_of_date:owner:title', [$page_owner->getDisplayName()]);
 
-$body = elgg_list_entities($options);
+$body = elgg_call(ELGG_IGNORE_ACCESS, function() use ($page_owner) {
+	$dbprefix = elgg_get_config('dbprefix');
+	$days = (int) elgg_get_plugin_setting('out_of_date_days', 'static');
+
+	return elgg_list_entities([
+		'type' => 'object',
+		'subtype' => StaticPage::SUBTYPE,
+		'modified_time_upper' => time() - ($days * 24 * 60 * 60),
+		'wheres' => [
+			new WhereClause("e.guid IN (
+				SELECT revs.entity_guid
+				FROM (
+					SELECT *
+					FROM {$dbprefix}annotations a
+					JOIN (
+						SELECT entity_guid AS e_guid, MAX(time_created) AS max_time
+		                FROM {$dbprefix}annotations
+		                WHERE name = 'static_revision'
+		                GROUP BY entity_guid
+					) AS b ON (
+	            		a.entity_guid = b.e_guid
+						AND a.time_created = b.max_time
+					)
+					WHERE a.owner_guid = {$page_owner->guid}
+				) revs
+			)"),
+		],
+		'order_by' => new OrderByClause('e.time_updated', 'DESC'),
+		'no_results' => elgg_echo('static:out_of_date:none'),
+	]);
+});
 
 // build page
 $page_data = elgg_view_layout('default', [
