91baa09f17f8a467303659fd3b054ecb1fb49851
Jerôme Bakker <jeabakker@coldtrick.com>
2025-06-18 14:09:53 +0200

    chore: wrapping up v13.1.3

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -14,7 +14,7 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
 
 return [
 	'plugin' => [
-		'version' => '13.1.2',
+		'version' => '13.1.3',
 	],
 	'bootstrap' => Bootstrap::class,
 	'settings' => [
